c9e9313892eca7715e7c27ee1e7b8b0c84737320
Alex Chen <lanyelg@gmail.com>
2022-03-21 19:23:41 +0800

    Add initial version VSCode with CMake on Linux.

---
+++ helloworld-linux-cmake/HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "HelloWorld.h"
+
+void HelloWorld::sayHello()
+{
+  std::cout<<"Hello World from VSCode CMake on Linux"<<std::endl;
+}
---
+++ helloworld-linux-cmake/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "HelloWorld.h"
+
+int main(int argc, char* argv[])
+{
+    HelloWorld helloworld;
+    helloworld.sayHello();
+    return 0;
+}
